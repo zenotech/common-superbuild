b1a013fd1f2f6f99dab6d0fc044a905c0735e9ce
Ben Boeckel <ben.boeckel@kitware.com>
2015-08-25 15:28:45 -0400

    cmake: refactor 64-bit build detection

--- cmake/SuperbuildUtils.cmake
+++ cmake/SuperbuildUtils.cmake
@@ -1,1 +1,20 @@
 include("SuperbuildUtils-apple")
+
+function (superbuild_detect_64bit_target)
+  if (CROSS_BUILD_STAGE STREQUAL "cross")
+    return ()
+  endif ()
+
+  # Collect information about the build platform.
+  include(CheckTypeSize)
+
+  check_type_size(void* void_ptr_size
+    BUILTIN_TYPES_ONLY)
+  if (void_ptr_size EQUAL 8)
+    set(64bit_build TRUE
+      PARENT_SCOPE)
+  else ()
+    set(64bit_build FALSE
+      PARENT_SCOPE)
+  endif ()
+endfunction ()
