57b1284e9e428b54a41d8354afdea029f9cd7ff7
T.J. Corona <tj.corona@kitware.com>
2015-08-28 09:44:09 -0400

    apple: detect OS X SDK version automatically

--- cmake/SuperbuildUtils-apple.cmake
+++ cmake/SuperbuildUtils-apple.cmake
@@ -6,37 +6,47 @@ function (superbuild_osx_determine_target)
   set(CMAKE_OSX_ARCHITECTURES "x86_64"
     CACHE STRING "By default, build for 64-bit Leopard")
 
-  if (CMAKE_OSX_SDK)
-    if (NOT CMAKE_OSX_SYSROOT)
-      execute_process(
-        COMMAND xcodebuild
-                -sdk "${CMAKE_OSX_SDK}"
-                -version Path
-        OUTPUT_VARIABLE
-                CMAKE_OSX_SYSROOT
-        RESULT_VARIABLE
+  if (NOT CMAKE_OSX_SDK)
+    execute_process(
+      COMMAND   xcrun
+                --show-sdk-version
+      OUTPUT_VARIABLE
+                CMAKE_OSX_SDK
+      RESULT_VARIABLE
                 res
-        OUTPUT_STRIP_TRAILING_WHITESPACE)
-      if (res)
-        message(FATAL_ERROR "Cannot determine SDK path for SDK: ${CMAKE_OSX_SDK}")
-      endif ()
+      OUTPUT_STRIP_TRAILING_WHITESPACE)
+    if (res)
+      message(FATAL_ERROR "Failed to detect CMAKE_OSX_SDK; please set manually (e.g., \"macosx10.9\").")
     endif ()
-    if (NOT CMAKE_OSX_DEPLOYMENT_TARGET)
-      execute_process(
-        COMMAND xcodebuild
-                -sdk "${CMAKE_OSX_SDK}"
-                -version SDKVersion
-        OUTPUT_VARIABLE
-                CMAKE_OSX_DEPLOYMENT_TARGET
-        RESULT_VARIABLE
-                res
-        OUTPUT_STRIP_TRAILING_WHITESPACE)
-      if (res)
-        message(FATAL_ERROR "Cannot determine SDK version for SDK: ${CMAKE_OSX_SDK}")
-      endif ()
+  endif ()
+
+  if (NOT CMAKE_OSX_SYSROOT)
+    execute_process(
+      COMMAND xcodebuild
+              -sdk "${CMAKE_OSX_SDK}"
+              -version Path
+      OUTPUT_VARIABLE
+              CMAKE_OSX_SYSROOT
+      RESULT_VARIABLE
+              res
+      OUTPUT_STRIP_TRAILING_WHITESPACE)
+    if (res)
+      message(FATAL_ERROR "Cannot determine SDK path for SDK: ${CMAKE_OSX_SDK}")
     endif ()
-  else ()
-    message(FATAL_ERROR "Ensure that CMAKE_OSX_SDK is set correctly")
+  endif ()
+  if (NOT CMAKE_OSX_DEPLOYMENT_TARGET)
+    execute_process(
+      COMMAND xcodebuild
+              -sdk "${CMAKE_OSX_SDK}"
+              -version SDKVersion
+      OUTPUT_VARIABLE
+              CMAKE_OSX_DEPLOYMENT_TARGET
+      RESULT_VARIABLE
+              res
+      OUTPUT_STRIP_TRAILING_WHITESPACE)
+    if (res)
+      message(FATAL_ERROR "Cannot determine SDK version for SDK: ${CMAKE_OSX_SDK}")
+    endif ()
   endif ()
 endfunction ()
 
