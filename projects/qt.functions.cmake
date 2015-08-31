811e49be74b4d5011ab5bdf08bf3f0045d969a23
Ben Boeckel <ben.boeckel@kitware.com>
2015-08-31 11:05:15 -0400

    qt: fix sanity check function

--- projects/qt.functions.cmake
+++ projects/qt.functions.cmake
@@ -1,4 +1,4 @@
-function (qt_sanity_check)
+function (superbuild_qt_sanity_check)
   if (qt4_enabled AND qt5_enabled)
     message(FATAL_ERROR "qt4 and qt5 cannot be enabled at the same time.")
   endif ()
