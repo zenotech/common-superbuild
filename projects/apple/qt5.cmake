57e92eff94f19c329a2b7d5f64c2903fa40286f3
Ben Boeckel <ben.boeckel@kitware.com>
2015-11-24 12:00:12 -0500

    qt5: fix syntax error

--- projects/apple/qt5.cmake
+++ projects/apple/qt5.cmake
@@ -7,7 +7,7 @@ include(qt5.common)
 # modal dialog problems
 # https://bugreports.qt.io/browse/QTBUG-40585
 superbuild_project_add_step(qt5-patch-qcocoaeventdispatcher
-  COMMAND "${CMAKE_COMMAND" -E copy_if_different
+  COMMAND "${CMAKE_COMMAND}" -E copy_if_different
           "${CMAKE_CURRENT_LIST_DIR}/patches/qt5.qtbase.plugins.platforms.cocoa.qcocoaeventdispatcher.mm"
           <SOURCE_DIR>/qtbase/plugins/platforms/cocoa/qcocoaeventdispatcher.mm
   DEPENDEES configure
