06c97c12aab0244fb7aad1c1bb04ba1b60d0b57b
Ben Boeckel <ben.boeckel@kitware.com>
2015-12-02 12:34:19 -0500

    zlib: turn version script fix into a patch

--- projects/zlib.cmake
+++ projects/zlib.cmake
@@ -9,12 +9,7 @@ superbuild_add_project(zlib
   CMAKE_ARGS
     -DBUILD_SHARED_LIBS:BOOL=${BUILD_SHARED_LIBS})
 
-#-------------------------------------------------------
 # zlib needs a fix for '--version-script' linker option. The option needs to be
-# removed on Macs. The fix to CMakeLists.txt works for all OSs.
-superbuild_project_add_step(zlib-patch-CMakeLists.txt
-  COMMAND "${CMAKE_COMMAND}" -E copy_if_different
-          "${CMAKE_CURRENT_LIST_DIR}/patches/zlib.CMakeLists.txt"
-          "<SOURCE_DIR>/CMakeLists.txt"
-  DEPENDEES update
-  DEPENDERS patch)
+# removed on Macs.
+superbuild_apply_patch(zlib version-script
+  "Remove --version-script linker argument on OS X")
