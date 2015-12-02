c4091d4ea24b93c43ffeb4d4c60110288f0853cb
Ben Boeckel <ben.boeckel@kitware.com>
2015-12-02 12:13:58 -0500

    szip: turn ricehdf.h installation into a patch

--- projects/win32/szip.cmake
+++ projects/win32/szip.cmake
@@ -1,12 +1,9 @@
 superbuild_add_project(szip
   CMAKE_ARGS
     -DBUILD_SHARED_LIBS:BOOL=ON
-    -DBUILD_TESTING:BOOL=OFF
-  PATCH_COMMAND
-    # BUG: szip does not install ricehdf.h
-    # replace the file with an appropriate version.
-    "${CMAKE_COMMAND}" -E copy_if_different
-      "${CMAKE_CURRENT_LIST_DIR}/patches/szip.src.CMakeLists.txt"
-      <SOURCE_DIR>/src/CMakeLists.txt)
+    -DBUILD_TESTING:BOOL=OFF)
+
+superbuild_apply_patch(szip install-ricehdf.h
+  "Fix CMake script to install ricehdf5.h")
 
 include(szip.common)
