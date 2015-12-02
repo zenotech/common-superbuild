a8d71a0bddcd40cfa907a0f4ee276b610655c041
Ben Boeckel <ben.boeckel@kitware.com>
2015-12-02 12:16:39 -0500

    szip: turn filename component fix into a patch

--- projects/szip.common.cmake
+++ projects/szip.common.cmake
@@ -1,11 +1,6 @@
-# get_filename_component on line 10 is missing the 'component' (path).
-# this patch fixes that.
-superbuild_project_add_step(szip-patch-install
-  COMMAND "${CMAKE_COMMAND}" -E copy_if_different
-          "${CMAKE_CURRENT_LIST_DIR}/patches/szip.config.cmake.SZIP-config.cmake.install.in"
-          <SOURCE_DIR>/config/cmake/SZIP-config.cmake.install.in
-  DEPENDEES update # do after update
-  DEPENDERS patch) # do before patch
+# get_filename_component on is missing the 'component' (path)
+superbuild_apply_patch(szip add-filename-component
+  "Fix get_filename_component call")
 
 superbuild_add_extra_cmake_args(
   -DSZIP_LIBRARY:FILEPATH=<INSTALL_DIR>/lib/${CMAKE_SHARED_LIBRARY_PREFIX}sz${CMAKE_SHARED_LIBRARY_SUFFIX}
