fd836d332e0e039d2113fb9929d9daa89c9c35eb
Ben Boeckel <ben.boeckel@kitware.com>
2015-12-07 16:33:15 -0500

    packaging: add a function for external package-making
    Some projects have more than one deliverable, but CPack only supports
    one at a time. Add a mechanism for creating projects which does nothing
    but set up CPack for a deliverable.

--- cmake/SuperbuildPackageMacros.cmake
+++ cmake/SuperbuildPackageMacros.cmake
@@ -1,14 +1,43 @@
+set(_superbuild_packaging_cmake_dir "${CMAKE_CURRENT_LIST_DIR}")
+
+function (superbuild_add_extra_package_test name generator)
+  set(cpack_working_dir "${CMAKE_BINARY_DIR}/cpack/${name}/${generator}")
+  configure_file(
+    "${_superbuild_packaging_cmake_dir}/superbuild_package_cmakelists.cmake.in"
+    "${cpack_working_dir}/CMakeLists.txt"
+    @ONLY)
+
+  add_test(
+    NAME    "cpack-${name}-${generator}"
+    COMMAND "${CMAKE_COMMAND}"
+            -Dname=${name}
+            -Dcmake_generator=${CMAKE_GENERATOR}
+            -Dcpack_generator=${generator}
+            -Doutput_directory=${CMAKE_BINARY_DIR}
+            -Dworking_directory=${cpack_working_dir}
+            -P "${_superbuild_packaging_cmake_dir}/superbuild_package_test.cmake"
+    WORKING_DIRECTORY "${cpack_working_dir}")
+
+  if (ARGN)
+    set_tests_properties("cpack-${name}-${generator}"
+      PROPERTIES
+        RESOURCE_LOCK cpack
+        ${ARGN})
+  endif ()
+endfunction ()
+
 function (superbuild_add_package_test generator)
   add_test(
     NAME    "cpack-${generator}"
     COMMAND "${CMAKE_CPACK_COMMAND}"
             -V
             -G "${generator}"
-    WORKING_DIRECTORY
-            "${CMAKE_BINARY_DIR}")
+    WORKING_DIRECTORY "${CMAKE_BINARY_DIR}")
 
   if (ARGN)
     set_tests_properties("cpack-${generator}"
-      PROPERTIES ${ARGN})
+      PROPERTIES
+        RESOURCE_LOCK cpack
+        ${ARGN})
   endif ()
 endfunction ()
