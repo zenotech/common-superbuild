94ecd16462073bc927262239cfc82f9c47434a65
Ben Boeckel <ben.boeckel@kitware.com>
2015-12-08 16:34:45 -0500

    SuperbuildTestingMacros: use a more specific name

--- cmake/SuperbuildTestingMacros.cmake
+++ cmake/SuperbuildTestingMacros.cmake
@@ -1,4 +1,4 @@
-set(_superbuild_cmake_dir "${CMAKE_CURRENT_LIST_DIR}")
+set(_superbuild_testing_cmake_dir "${CMAKE_CURRENT_LIST_DIR}")
 
 function (superbuild_add_extract_test generator output)
   set(_ZIP_test_glob "*.zip")
@@ -12,7 +12,7 @@ function (superbuild_add_extract_test generator output)
             -Dtest_dir:PATH=${CMAKE_BINARY_DIR}
             -Dbinary_glob:STRING=${_${generator}_test_glob}
             -Doutput_dir:PATH=${output}
-            -P "${_superbuild_cmake_dir}/superbuild_testing_extract_binary.cmake")
+            -P "${_superbuild_testing_cmake_dir}/superbuild_testing_extract_binary.cmake")
   set_tests_properties("extract-${generator}"
     PROPERTIES
       DEPENDS "cpack-${generator}"
