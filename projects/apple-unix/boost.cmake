362bcd370a1068a68361274a698985722da63435
Ben Boeckel <ben.boeckel@kitware.com>
2015-12-09 18:21:01 -0500

    boost: update to 1.59.0

--- projects/apple-unix/boost.cmake
+++ projects/apple-unix/boost.cmake
@@ -5,3 +5,6 @@ if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
 endif ()
 
 include(boost.common)
+
+superbuild_apply_patch(boost osx-rpath
+    "Remove @rpath from the install name of Boost's libraries")
