c5535b80b8ccb4012f79c7801ef56c20bb1c47fa
Ben Boeckel <ben.boeckel@kitware.com>
2016-03-17 15:13:12 -0400

    szip: add win32 patch
    The newline endings are different.

--- projects/szip.common.cmake
+++ projects/szip.common.cmake
@@ -1,6 +1,11 @@
 # get_filename_component on is missing the 'component' (path)
-superbuild_apply_patch(szip add-filename-component
-  "Fix get_filename_component call")
+if (WIN32)
+  superbuild_apply_patch(szip add-filename-component-win32
+    "Fix get_filename_component call")
+else ()
+  superbuild_apply_patch(szip add-filename-component
+    "Fix get_filename_component call")
+endif ()
 
 superbuild_add_extra_cmake_args(
   -DSZIP_LIBRARY:FILEPATH=<INSTALL_DIR>/lib/${CMAKE_SHARED_LIBRARY_PREFIX}sz${CMAKE_SHARED_LIBRARY_SUFFIX}
