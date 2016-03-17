c77edf8b5c67b4c3a0a5e7a39715992d94222db4
Ben Boeckel <ben.boeckel@kitware.com>
2016-03-17 17:21:28 -0400

    hdf5: fix cmake variables on Windows

--- projects/win32/hdf5.cmake
+++ projects/win32/hdf5.cmake
@@ -4,15 +4,15 @@ include("${CMAKE_CURRENT_LIST_DIR}/../hdf5.cmake")
 # the dlls instead of the libs. So setting the variables explicitly for
 # dependent projects.
 superbuild_add_extra_cmake_args(
-  -DHDF5_C_LIBRARY:FILEPATH=${install_location}/lib/hdf5.lib
-  -DHDF5_HL_LIBRARY:FILEPATH=${install_location}/lib/hdf5_hl.lib
+  -DHDF5_C_LIBRARY:FILEPATH=<INSTALL_DIR>/lib/hdf5.lib
+  -DHDF5_HL_LIBRARY:FILEPATH=<INSTALL_DIR>/lib/hdf5_hl.lib
   # This variable is for CGNS, since CGNS doesn't use standard find_package()
   # to find hdf5.
-  -DHDF5_LIBRARY:FILEPATH=${install_location}/lib/hdf5.lib
+  -DHDF5_LIBRARY:FILEPATH=<INSTALL_DIR>/lib/hdf5.lib
   # These variables are for netcdf
-  -DHDF5_LIB:FILEPATH=${install_location}/lib/hdf5.lib
-  -DHDF5_HL_LIB:FILEPATH=${install_location}/lib/hdf5_hl.lib
-  -DHDF5_INCLUDE_DIR:FILEPATH=${install_location}/include)
+  -DHDF5_LIB:FILEPATH=<INSTALL_DIR>/lib/hdf5.lib
+  -DHDF5_HL_LIB:FILEPATH=<INSTALL_DIR>/lib/hdf5_hl.lib
+  -DHDF5_INCLUDE_DIR:FILEPATH=<INSTALL_DIR>/include)
 
 if (NOT superbuild_is_64bit)
   # On 32-bit Windows, H5public.h ends up redefining ssize_t. This patch ensures
