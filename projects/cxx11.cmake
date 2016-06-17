b7bc0f781368060b5676eec45adc23258502211d
Ben Boeckel <ben.boeckel@kitware.com>
2016-06-17 15:37:56 -0400

    cxx11: CMAKE_CXX_STANDARD_REQUIRED is a bool

--- projects/cxx11.cmake
+++ projects/cxx11.cmake
@@ -2,6 +2,6 @@ superbuild_add_dummy_project(cxx11)
 
 superbuild_add_extra_cmake_args(
   -DCMAKE_CXX_STANDARD:STRING=11
-  -DCMAKE_CXX_STANDARD_REQUIRED:STRING=11)
+  -DCMAKE_CXX_STANDARD_REQUIRED:STRING=TRUE)
 
 superbuild_append_flags(cxx_flags "${CMAKE_CXX11_STANDARD_COMPILE_OPTION}")
