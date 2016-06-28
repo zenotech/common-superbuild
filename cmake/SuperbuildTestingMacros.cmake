c4bdbeabc6a1f0f5ebc3666f6835eb5848fafcd8
Ben Boeckel <ben.boeckel@kitware.com>
2016-06-28 10:56:12 -0400

    SuperbuildTestingMacros: support extracting specific tarballs
    Now that the packaging logic supports multiple packages, the blind glob
    is not precise enough.

--- cmake/SuperbuildTestingMacros.cmake
+++ cmake/SuperbuildTestingMacros.cmake
@@ -1,20 +1,21 @@
 set(_superbuild_testing_cmake_dir "${CMAKE_CURRENT_LIST_DIR}")
 
-function (superbuild_add_extract_test generator output)
-  set(_ZIP_test_glob "*.zip")
-  set(_NSIS_test_glob "*.exe")
-  set(_DragNDrop_test_glob "*.dmg")
-  set(_TGZ_test_glob "*.tar.gz")
+function (superbuild_add_extract_test name glob_prefix generator output)
+  set(_ZIP_test_glob "${glob_prefix}*.zip")
+  set(_NSIS_test_glob "${glob_prefix}*.exe")
+  set(_DragNDrop_test_glob "${glob_prefix}*.dmg")
+  set(_TGZ_test_glob "${glob_prefix}*.tar.gz")
 
   add_test(
-    NAME    "extract-${generator}"
+    NAME    "extract-${name}-${generator}"
     COMMAND "${CMAKE_COMMAND}"
-            -Dtest_dir:PATH=${CMAKE_BINARY_DIR}
-            -Dbinary_glob:STRING=${_${generator}_test_glob}
-            -Doutput_dir:PATH=${output}
+            "-Dname:STRING=${name}"
+            "-Dtest_dir:PATH=${CMAKE_BINARY_DIR}"
+            "-Dbinary_glob:STRING=${_${generator}_test_glob}"
+            "-Doutput_dir:PATH=${output}"
             -P "${_superbuild_testing_cmake_dir}/scripts/extract_test.cmake")
-  set_tests_properties("extract-${generator}"
+  set_tests_properties("extract-${name}-${generator}"
     PROPERTIES
-      DEPENDS "cpack-${generator}"
+      DEPENDS "cpack-${name}-${generator}"
       ${ARGN})
 endfunction ()
