e0648fb60850809a4e46a0b10992b79e5d9ff3e7
Ben Boeckel <ben.boeckel@kitware.com>
2016-07-07 16:42:11 -0400

    prebuilt-python: install the python binary

--- prebuilt-python/projects/win32/python.bundle.cmake
+++ prebuilt-python/projects/win32/python.bundle.cmake
@@ -2,5 +2,7 @@ set(package_name python)
 set(version 2.7.2)
 include(python.bundle.common)
 
+superbuild_windows_install_program(python "")
+
 include(python.functions)
 superbuild_install_superbuild_python()
