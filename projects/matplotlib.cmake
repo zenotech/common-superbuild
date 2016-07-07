d8614c94f740c264eaf68342c85246befce033ed
Ben Boeckel <ben.boeckel@kitware.com>
2016-07-07 09:58:14 -0400

    matplotlib: depend on pythonsetuptools

--- projects/matplotlib.cmake
+++ projects/matplotlib.cmake
@@ -1,6 +1,6 @@
 superbuild_add_project(matplotlib
   CAN_USE_SYSTEM
-  DEPENDS python numpy png freetype zlib
+  DEPENDS python numpy png freetype zlib pythonsetuptools
   BUILD_IN_SOURCE 1
   CONFIGURE_COMMAND
     "${CMAKE_COMMAND}"
