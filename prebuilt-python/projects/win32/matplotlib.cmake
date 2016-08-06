574320a8974352f42bad9c074bf234c1bb11ca00
Ben Boeckel <ben.boeckel@kitware.com>
2016-08-06 17:54:36 -0400

    prebuilt-python: use the common matplotlib build file

--- prebuilt-python/projects/win32/matplotlib.cmake
+++ prebuilt-python/projects/win32/matplotlib.cmake
@@ -1,18 +1,1 @@
-superbuild_add_project(matplotlib
-  CAN_USE_SYSTEM
-  DEPENDS python numpy png freetype
-  BUILD_IN_SOURCE 1
-  CONFIGURE_COMMAND
-    "${CMAKE_COMMAND}"
-      "-DPATCHES_DIR:PATH=${common-superbuild_SOURCE_DIR}/projects/patches"
-      "-DSOURCE_DIR:PATH=<SOURCE_DIR>"
-      "-DINSTALL_DIR:PATH=<INSTALL_DIR>"
-      -P "${common-superbuild_SOURCE_DIR}/projects/scripts/matplotlib.patch.cmake"
-  BUILD_COMMAND
-    "${CMAKE_COMMAND}"
-      "-DPYTHON_EXECUTABLE:PATH=${superbuild_python_executable}"
-      "-DSOURCE_DIR:PATH=<SOURCE_DIR>"
-      "-DINSTALL_DIR:PATH=<INSTALL_DIR>"
-      "-DINSTALL_PREFIX:PATH=<INSTALL_DIR>/bin"
-      -P "${common-superbuild_SOURCE_DIR}/projects/scripts/matplotlib.build.cmake"
-  INSTALL_COMMAND "")
+include("${common-superbuild_SOURCE_DIR}/projects/matplotlib.cmake")
