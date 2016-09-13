28810547415d601dfd22b8a7458eb9db1e89f6d6
Ben Boeckel <ben.boeckel@kitware.com>
2016-09-13 10:29:47 -0400

    tbb: inform tbb about gcc 5.1 stdlib features
    In ParaView, ospray is dumping out multiple MBs of log output due to a
    deprecation warning with this not fixed.

--- projects/unix/tbb.cmake
+++ projects/unix/tbb.cmake
@@ -4,6 +4,9 @@ else ()
   set(tbb_archdir ia32)
 endif ()
 
-set(tbb_libdir lib/${tbb_archdir}/gcc4.4)
+set(tbb_libdir "lib/${tbb_archdir}/gcc4.4")
 set(tbb_libsuffix "${CMAKE_SHARED_LIBRARY_SUFFIX}*")
 include(tbb.common)
+
+superbuild_apply_patch(tbb gcc5x-warning-fix
+  "Tell TBB about GCC 5.1 stdlib support")
