1fb14baa5d14269bbe58c14ce28d25f929d5e86e
Ben Boeckel <ben.boeckel@kitware.com>
2016-09-22 10:20:32 -0400

    qt4: remove old workaround option
    We no longer build on Debian 6, so we no longer need this fix.
    
    Fixes #21.

--- projects/qt4.common.cmake
+++ projects/qt4.common.cmake
@@ -2,23 +2,9 @@ set(qt4_EXTRA_CONFIGURATION_OPTIONS ""
   CACHE STRING "Extra arguments to be passed to Qt4 when configuring.")
 mark_as_advanced(qt4_EXTRA_CONFIGURATION_OPTIONS)
 
-# See https://bugreports.qt.io/browse/QTBUG-5774 and the links available there.
-cmake_dependent_option(qt4_WORK_AROUND_BROKEN_ASSISTANT_BUILD "Work around a build issue in Qt. Use this if you see linker errors with QtHelp and QCLucene." OFF
-  "NOT WIN32" OFF)
-mark_as_advanced(qt4_WORK_AROUND_BROKEN_ASSISTANT_BUILD)
-
 set(qt4_build_commands
   BUILD_COMMAND   $(MAKE)
   INSTALL_COMMAND make install)
-if (qt4_WORK_AROUND_BROKEN_ASSISTANT_BUILD)
-  # This hack is required because Qt's build gets mucked up when we set
-  # LDFLAGS, CXXFLAGS, etc. Installing things makes it work because the files
-  # get placed into the install tree which has rpaths so they get found. Since
-  # it is such a hack, it is an option which off and hidden by default.
-  set(qt4_build_commands
-    BUILD_COMMAND   $(MAKE) install
-    INSTALL_COMMAND "")
-endif ()
 
 set(qt4_configure_ext)
 if (WIN32)
