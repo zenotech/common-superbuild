d98e3a0686a2974dfa6297cd1c893f43ca9e4d13
Ben Boeckel <ben.boeckel@kitware.com>
2016-09-23 13:30:09 -0400

    szip: simplify installation

--- projects/apple-unix/szip.cmake
+++ projects/apple-unix/szip.cmake
@@ -15,4 +15,9 @@ superbuild_add_project(szip
   INSTALL_COMMAND
     make install)
 
-include(szip.common)
+superbuild_apply_patch(szip add-filename-component
+  "Fix get_filename_component call")
+
+superbuild_add_extra_cmake_args(
+  -DSZIP_LIBRARY:FILEPATH=<INSTALL_DIR>/lib/${CMAKE_SHARED_LIBRARY_PREFIX}sz${CMAKE_SHARED_LIBRARY_SUFFIX}
+  -DSZIP_INCLUDE_DIR:FILEPATH=<INSTALL_DIR>/include)
