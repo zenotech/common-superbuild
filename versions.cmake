f265cf2e731134877ed6f4cbe0436a43243bf39c
Chuck Atkins <chuck.atkins@kitware.com>
2016-11-23 10:19:48 -0500

    Versions: Fix all download URLs to point to www.paraview.org

--- versions.cmake
+++ versions.cmake
@@ -20,11 +20,11 @@ superbuild_set_revision(zlib
   URL_MD5 60df6a37c56e7c1366cca812414f7b85)
 
 superbuild_set_revision(ffmpeg
-  URL     "http://paraview.org/files/dependencies/ffmpeg-2.3.3.tar.bz2"
+  URL     "http://www.paraview.org/files/dependencies/ffmpeg-2.3.3.tar.bz2"
   URL_MD5 72361d3b8717b6db3ad2b9da8df7af5e)
 
 superbuild_set_revision(szip
-  URL     "http://paraview.org/files/dependencies/szip-2.1.tar.gz"
+  URL     "http://www.paraview.org/files/dependencies/szip-2.1.tar.gz"
   URL_MD5 902f831bcefb69c6b635374424acbead)
 
 superbuild_set_revision(hdf5
@@ -51,24 +51,24 @@ if (WIN32)
   endif ()
 else()
   superbuild_set_revision(python
-    URL     "http://paraview.org/files/dependencies/Python-2.7.11.tgz"
+    URL     "http://www.paraview.org/files/dependencies/Python-2.7.11.tgz"
     URL_MD5 6b6076ec9e93f05dd63e47eb9c15728b)
 endif()
 
 superbuild_set_revision(ftjam
-  URL     "http://paraview.org/files/dependencies/ftjam-2.5.2-win32.tar.bz2"
+  URL     "http://www.paraview.org/files/dependencies/ftjam-2.5.2-win32.tar.bz2"
   URL_MD5 ee52f3faff6d31ffb89a2fedb3b0caf6)
 
 superbuild_set_revision(freetype
-  URL     "http://paraview.org/files/dependencies/freetype-2.4.8.tar.gz"
+  URL     "http://www.paraview.org/files/dependencies/freetype-2.4.8.tar.gz"
   URL_MD5 5d82aaa9a4abc0ebbd592783208d9c76)
 
 superbuild_set_revision(fontconfig
-  URL     "http://paraview.org/files/dependencies/fontconfig-2.12.1.tar.bz2"
+  URL     "http://www.paraview.org/files/dependencies/fontconfig-2.12.1.tar.bz2"
   URL_MD5 b5af5a423ee3b5cfc34846838963c058)
 
 superbuild_set_revision(libxml2
-  URL     "http://paraview.org/files/dependencies/libxml2-2.7.8.tar.gz"
+  URL     "http://www.paraview.org/files/dependencies/libxml2-2.7.8.tar.gz"
   URL_MD5 8127a65e8c3b08856093099b52599c86)
 
 if (WIN32)
@@ -79,7 +79,7 @@ else ()
   set(qt4_md5 "2edbe4d6c2eff33ef91732602f3518eb")
 endif ()
 superbuild_set_revision(qt4
-  URL     "http://paraview.org/files/dependencies/qt-everywhere-opensource-src-${qt4_ver}.tar.gz"
+  URL     "http://www.paraview.org/files/dependencies/qt-everywhere-opensource-src-${qt4_ver}.tar.gz"
   URL_MD5 "${qt4_md5}")
 
 set(qt5_ver_series "5.7")
@@ -88,67 +88,67 @@ if (WIN32)
   set(qt5_ext "zip")
   set(qt5_md5 "c5d4bb1d015c2cfd37d183e5d201051f")
 else ()
-  set(qt5_ext "tar.gz")
-  set(qt5_md5 "9a46cce61fc64c20c3ac0a0e0fa41b42")
+  set(qt5_ext "tar.xz")
+  set(qt5_md5 "63ec6b584757eef8cd713e4958297251")
 endif ()
 superbuild_set_revision(qt5
-  URL     "http://download.qt.io/official_releases/qt/${qt5_ver_series}/${qt5_ver}/single/qt-everywhere-opensource-src-${qt5_ver}.${qt5_ext}"
+  URL     "http://www.paraview.org/files/dependencies/qt-everywhere-opensource-src-${qt5_ver}.${qt5_ext}"
   URL_MD5 "${qt5_md5}")
 
 if (WIN32 AND NOT superbuild_building_prebuilt_python)
   if (superbuild_is_64bit)
     superbuild_set_revision(numpy
-      URL     "http://paraview.org/files/dependencies/numpy-1.8.1-win64-20160927.tar.gz"
+      URL     "http://www.paraview.org/files/dependencies/numpy-1.8.1-win64-20160927.tar.gz"
       URL_MD5 c03c7f0d9e88b9d0d2eaedb4571dab90)
   else ()
     superbuild_set_revision(numpy
-      URL     "http://paraview.org/files/dependencies/numpy-1.8.1-win32-20160927.tar.gz"
+      URL     "http://www.paraview.org/files/dependencies/numpy-1.8.1-win32-20160927.tar.gz"
       URL_MD5 548f894f23dfd87de14191a8425eb6e0)
   endif ()
 else ()
   superbuild_set_revision(numpy
-    URL     "http://paraview.org/files/dependencies/numpy-1.8.1+cmake+static.tar.bz2"
+    URL     "http://www.paraview.org/files/dependencies/numpy-1.8.1+cmake+static.tar.bz2"
     URL_MD5 1974dbb4bfa1509e492791a8cd225774)
 endif ()
 
 if (WIN32 AND NOT superbuild_building_prebuilt_python)
   if (superbuild_is_64bit)
     superbuild_set_revision(matplotlib
-      URL     "http://paraview.org/files/dependencies/matplotlib-1.1.1-win64-20160927.tar.gz"
+      URL     "http://www.paraview.org/files/dependencies/matplotlib-1.1.1-win64-20160927.tar.gz"
       URL_MD5 cf20ab4210fe1ebd541ab04221d93e46)
   else ()
     superbuild_set_revision(matplotlib
-      URL     "http://paraview.org/files/dependencies/matplotlib-1.1.1-win32-20160927.tar.gz"
+      URL     "http://www.paraview.org/files/dependencies/matplotlib-1.1.1-win32-20160927.tar.gz"
       URL_MD5 c14d6c4f43f2bba537c027035c8e195a)
   endif ()
 else ()
   superbuild_set_revision(matplotlib
-    URL     "http://paraview.org/files/dependencies/matplotlib-1.1.1_notests.tar.gz"
+    URL     "http://www.paraview.org/files/dependencies/matplotlib-1.1.1_notests.tar.gz"
     URL_MD5 30ee59119599331bf1f3b6e838fee9a8)
 endif ()
 
 if (WIN32 AND NOT superbuild_building_prebuilt_python)
   if (superbuild_is_64bit)
     superbuild_set_revision(pywin32
-      URL     "http://paraview.org/files/dependencies/pywin32-220-win64-20160927.tar.gz"
+      URL     "http://www.paraview.org/files/dependencies/pywin32-220-win64-20160927.tar.gz"
       URL_MD5 064c396b315d2c1a11096df9378a503a)
   else ()
     superbuild_set_revision(pywin32
-      URL     "http://paraview.org/files/dependencies/pywin32-220-win32-20160927.tar.gz"
+      URL     "http://www.paraview.org/files/dependencies/pywin32-220-win32-20160927.tar.gz"
       URL_MD5 83ce04df1d0084b7adb1c94a8734423e)
   endif ()
 else ()
   superbuild_set_revision(pywin32
-    URL     "https://sourceforge.net/projects/pywin32/files/pywin32/Build%20220/pywin32-220.zip/download"
+    URL     "http://www.paraview.org/files/dependencies/pywin32-220.zip"
     URL_MD5 9c386839c1485b2047c03fab66e69b9e)
 endif ()
 
 superbuild_set_revision(mpi
-  URL     "http://paraview.org/files/dependencies/mpich2-1.4.1p1.tar.gz"
+  URL     "http://www.paraview.org/files/dependencies/mpich2-1.4.1p1.tar.gz"
   URL_MD5 b470666749bcb4a0449a072a18e2c204)
 
 superbuild_set_revision(lapack
-  URL     "http://paraview.org/files/dependencies/lapack-3.4.2.tgz"
+  URL     "http://www.paraview.org/files/dependencies/lapack-3.4.2.tgz"
   URL_MD5 61bf1a8a4469d4bdb7604f5897179478)
 
 # TODO: split into netcdf and netcdfcpp
@@ -174,5 +174,5 @@ superbuild_set_revision(tbb
   URL_MD5 "${tbb_md5}")
 
 superbuild_set_revision(pythonsetuptools
-  URL     "https://pypi.python.org/packages/45/5e/79ca67a0d6f2f42bfdd9e467ef97398d6ad87ee2fa9c8cdf7caf3ddcab1e/setuptools-23.0.0.tar.gz"
+  URL     "http://www.paraview.org/files/dependencies/setuptools-23.0.0.tar.gz"
   URL_MD5 100a90664040f8ff232fbac02a4c5652)
