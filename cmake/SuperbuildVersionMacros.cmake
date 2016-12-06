a0636af3c94c7abd751b5678e9cbd04c357a6d8f
Chuck Atkins <chuck.atkins@kitware.com>
2016-12-06 16:03:49 -0500

    superbuild: Fix version macros when using "source" selection

--- cmake/SuperbuildVersionMacros.cmake
+++ cmake/SuperbuildVersionMacros.cmake
@@ -88,7 +88,7 @@ endmacro ()
 
 function (superbuild_set_version_variables project default include_file)
   set(source_dir "")
-  if (${project}_FROM_SOURCE_DIR)
+  if (${project}_SOURCE_SELECTION STREQUAL "source")
     set(source_dir "${${project}_SOURCE_DIR}")
   endif ()
   superbuild_detect_version_git("${project}" "${source_dir}" "${default}")
