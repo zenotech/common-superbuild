f64e51c25c8078c41a4ba0df8acf5781efe16599
TJ Corona <tj.corona@khq.kitware.com>
2016-12-09 09:27:13 -0500

    Qt: Avoid redefining snprintf for VS2015; ignore dep warning
    Also, rename template parameters that are reserved by C++.

--- projects/win32/qt4.cmake
+++ projects/win32/qt4.cmake
@@ -1,1 +1,13 @@
+set(qt4_extra_options)
+
+# Set the silence flag to remove a warning in 2015 that is really an error.
+if (MSVC14)
+  list(APPEND qt4_extra_options
+    -D _SILENCE_STDEXT_HASH_DEPRECATION_WARNINGS)
+endif ()
+
 include(qt4.common)
+
+# see https://connect.microsoft.com/VisualStudio/feedback/details/1424082/vs2015-rc-c-compiler-mixes-up-template-parameter-with-identical-named-inherited-typedef
+superbuild_apply_patch(qt4 VS2015
+  "Replace reserved variables that conflict with VS2015")
