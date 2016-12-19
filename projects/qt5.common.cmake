2a3e582432553481283da4a9ba200e6c6ec69ec1
Ben Boeckel <ben.boeckel@kitware.com>
2016-12-19 10:53:13 -0500

    qt5: require cxx11

--- projects/qt5.common.cmake
+++ projects/qt5.common.cmake
@@ -51,8 +51,7 @@ endif ()
 
 superbuild_add_project(qt5
   CAN_USE_SYSTEM
-  DEPENDS ${qt5_depends} ${qt5_extra_depends}
-  DEPENDS_OPTIONAL cxx11
+  DEPENDS ${qt5_depends} ${qt5_extra_depends} cxx11
   CONFIGURE_COMMAND
     <SOURCE_DIR>/configure${qt5_configure_ext}
       -opensource
