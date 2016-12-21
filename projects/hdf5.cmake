94e658587351f7161f9cfc8441c8b82ca606477d
Ben Boeckel <ben.boeckel@kitware.com>
2016-12-21 12:14:01 -0500

    hdf5: set HDF5_DIR for dependent projects

--- projects/hdf5.cmake
+++ projects/hdf5.cmake
@@ -10,8 +10,14 @@ superbuild_add_project(hdf5
     -DHDF5_BUILD_HL_LIB:BOOL=TRUE
     -DHDF5_BUILD_WITH_INSTALL_NAME:BOOL=ON)
 
+if (WIN32)
+  set(hdf5_cmake_subdir "cmake/hdf5")
+else ()
+  set(hdf5_cmake_subdir "share/cmake/hdf5")
+endif ()
+
 superbuild_add_extra_cmake_args(
-  -DVTK_USE_SYSTEM_HDF5:BOOL=ON)
+  "-DHDF5_DIR:PATH=<INSTALL_DIR>/${hdf5_cmake_subdir}")
 
 # HDF5 on Windows installs a zlib that gets found this way; avoid using it.
 superbuild_apply_patch(hdf5 no-zlib-config
