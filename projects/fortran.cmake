d625f13c5849f0ea8dcd9c25587dcde62c3c82b7
Ben Boeckel <ben.boeckel@kitware.com>
2017-01-05 11:14:15 -0500

    fortran: defer enabling the language until it is necessary

--- projects/fortran.cmake
+++ projects/fortran.cmake
@@ -1,6 +1,8 @@
-enable_language(Fortran)
-
 superbuild_add_dummy_project(fortran)
+
+if (fortran_enabled)
+  enable_language(Fortran)
+endif ()
 
 superbuild_add_extra_cmake_args(
   -DCMAKE_Fortran_COMPILER:FILEPATH=${CMAKE_Fortran_COMPILER})
