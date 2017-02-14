4741ca0955ce3eb7cad06cf8fd3d429a35df63e8
Ben Boeckel <ben.boeckel@kitware.com>
2017-02-14 13:52:36 -0500

    prebuilt-python: simplify the package name logic

--- prebuilt-python/projects/win32/python.bundle.common.cmake
+++ prebuilt-python/projects/win32/python.bundle.common.cmake
@@ -10,15 +10,10 @@ set(CPACK_PACKAGE_INSTALL_DIRECTORY
 set(CPACK_SOURCE_PACKAGE_FILE_NAME
   "${CPACK_PACKAGE_NAME}-${version}")
 
-if (NOT DEFINED CPACK_SYSTEM_NAME)
-  set(CPACK_SYSTEM_NAME ${CMAKE_SYSTEM_NAME}-${CMAKE_SYSTEM_PROCESSOR})
-endif ()
-if (CPACK_SYSTEM_NAME MATCHES "Windows")
-  if (superbuild_is_64bit)
-    set(CPACK_SYSTEM_NAME win64)
-  else ()
-    set(CPACK_SYSTEM_NAME win32)
-  endif ()
+if (superbuild_is_64bit)
+  set(CPACK_SYSTEM_NAME "win64")
+else ()
+  set(CPACK_SYSTEM_NAME "win32")
 endif ()
 if (NOT DEFINED CPACK_PACKAGE_FILE_NAME)
   set(CPACK_PACKAGE_FILE_NAME
