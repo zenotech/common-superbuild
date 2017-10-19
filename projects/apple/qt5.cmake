6d4b78cac74a3765e626b10ac9ef5a65e65f86a4
Shawn Waldon <shawn.waldon@kitware.com>
2017-10-19 15:30:49 -0400

    qt5: remove modal dialog patch, 5.9.2 has a fix

--- projects/apple/qt5.cmake
+++ projects/apple/qt5.cmake
@@ -8,9 +8,4 @@ list(APPEND qt5_extra_options
 
 include(qt5.common)
 
-# Modal dialog problems
-# https://bugreports.qt.io/browse/QTBUG-40585
-superbuild_apply_patch(qt5 osx-10.9-modal-dialog
-  "Fix modal dialog state management for 10.9 and up")
-
 superbuild_append_flags(ld_flags "-headerpad_max_install_names" PROJECT_ONLY)
