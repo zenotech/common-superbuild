cacae76054eaab515ce3a7f64433dae41219f82b
Ben Boeckel <ben.boeckel@kitware.com>
2017-12-08 12:04:12 -0500

    prebuilt-python: skip the MSVC runtime
    Fixes #36

--- prebuilt-python/projects/win32/python.bundle.common.cmake
+++ prebuilt-python/projects/win32/python.bundle.common.cmake
@@ -19,3 +19,6 @@ if (NOT DEFINED CPACK_PACKAGE_FILE_NAME)
   set(CPACK_PACKAGE_FILE_NAME
     "${CPACK_SOURCE_PACKAGE_FILE_NAME}-${CPACK_SYSTEM_NAME}")
 endif ()
+
+# Skip installing the MSVC runtime.
+set(superbuild_bundle_skip_system_libraries TRUE)
