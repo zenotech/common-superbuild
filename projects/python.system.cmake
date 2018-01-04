a1f59b514637b0355b419477ba6992bd7314a515
Andrew Bauer <andy.bauer@kitware.com>
2018-01-04 18:35:01 -0500

    Make finding Python libs optional
    In certain cases we want the Python interpreter to do other build
    operations but we don't need the Python libraries. If a system
    Python interpreter is available but not the Python libraries, without
    this change we're foced to build Python in order to get both when we
    really only want the interpreter.

--- projects/python.system.cmake
+++ projects/python.system.cmake
@@ -1,19 +1,28 @@
-find_package(PythonLibs   2.7 REQUIRED)
+option(python_FIND_LIBRARIES "Require system Python development files" ON)
+mark_as_advanced(python_FIND_LIBRARIES)
+
 find_package(PythonInterp 2.7 REQUIRED)
 
-if (NOT PYTHON_VERSION_STRING STREQUAL PYTHONLIBS_VERSION_STRING AND
-    NOT PYTHON_I_KNOW_WHAT_IM_DOING)
-  message(FATAL_ERROR
-    "Python interpreter version (${PYTHON_VERSION_STRING}) does "
-    "not match the library version (${PYTHONLIBS_VERSION_STRING})")
-endif ()
+if (python_FIND_LIBRARIES)
+  find_package(PythonLibs   2.7 REQUIRED)
+  if (NOT PYTHON_VERSION_STRING STREQUAL PYTHONLIBS_VERSION_STRING AND
+      NOT PYTHON_I_KNOW_WHAT_IM_DOING)
+    message(FATAL_ERROR
+      "Python interpreter version (${PYTHON_VERSION_STRING}) does "
+      "not match the library version (${PYTHONLIBS_VERSION_STRING})")
+  endif ()
+endif()
 
 # This will add PYTHON_LIBRARY, PYTHON_EXECUTABLE, PYTHON_INCLUDE_DIR
 # variables. User can set/override these to change the Python being used.
 superbuild_add_extra_cmake_args(
-  -DPYTHON_EXECUTABLE:FILEPATH=${PYTHON_EXECUTABLE}
-  -DPYTHON_INCLUDE_DIR:PATH=${PYTHON_INCLUDE_DIR}
-  -DPYTHON_LIBRARY:FILEPATH=${PYTHON_LIBRARY})
+  -DPYTHON_EXECUTABLE:FILEPATH=${PYTHON_EXECUTABLE})
+
+if (python_FIND_LIBRARIES)
+  superbuild_add_extra_cmake_args(
+    -DPYTHON_INCLUDE_DIR:PATH=${PYTHON_INCLUDE_DIR}
+    -DPYTHON_LIBRARY:FILEPATH=${PYTHON_LIBRARY})
+endif()
 
 set(superbuild_python_executable "${PYTHON_EXECUTABLE}"
   CACHE INTERNAL "")
