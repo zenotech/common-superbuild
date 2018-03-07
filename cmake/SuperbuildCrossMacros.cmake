fe0684237577d02d720cc144592e0e6c96460b4a
Ben Boeckel <ben.boeckel@kitware.com>
2018-03-07 17:01:58 -0500

    docs: document SuperbuildCrossMacros

--- cmake/SuperbuildCrossMacros.cmake
+++ cmake/SuperbuildCrossMacros.cmake
@@ -1,3 +1,25 @@
+#[==[.md
+# Cross compilation support
+
+This is untested, but exists from prior support in a previous implemenation of
+the superbuild. Most of them are intended to be used by the common superbuild
+itself, but there are some hooks projects can set.
+
+It seems that the module is largely ignored for now.
+#]==]
+
+#[==[.md
+## Hooks
+
+### `superbuild_cross_prepare`
+
+If defined, this command is called in order to to do any up-front configuration
+settings which may be necessary.
+#]==]
+
+# Sets up the build for a particular target platform.
+#
+# Currently unused.
 function (superbuild_cross_determine_target)
   if (NOT CMAKE_CROSSCOMPILING)
     return ()
@@ -7,8 +29,7 @@ function (superbuild_cross_determine_target)
   # right environment settings.
   _superbuild_cross_target_machine()
 
-  # Configure the platform dependent settings 64bit_build, static_only, mpi
-  # search path.
+  # Configure platform dependent settings 64bit_build, static_only.
   _superbuild_cross_platform_settings()
 
   if (COMMAND superbuild_cross_prepare)
@@ -16,10 +37,7 @@ function (superbuild_cross_determine_target)
   endif ()
 endfunction ()
 
-#=============================================================================
-# Ask user what the target machine is, so that we can choose the right
-# right build hints and patches later on.
-#
+# Provide a cache variable to indicate the targeted machine.
 function (_superbuild_cross_target_machine)
   set(SUPERBUILD_CROSS_TARGET "generic"
     CACHE STRING "Platform to cross compile for, either generic|bgp_xlc|bgq_xlc|bgq_gnu|xk7_gnu")
@@ -28,8 +46,9 @@ function (_superbuild_cross_target_machine)
     "generic" "bgp_xlc" "bgq_xlc" "bgq_gnu" "xk7_gnu")
 endfunction ()
 
-#=============================================================================
-# Includes an optionally site-specific file from the cross-compiling directory.
+# Includes an optional site-specific file from the cross-compiling directory.
+#
+# Currently unused.
 function (_superbuild_cross_include_file var name)
   set(site_file
     "crosscompile/${SUPERBUILD_CROSS_TARGET}/${name}.cmake")
@@ -44,10 +63,8 @@ function (_superbuild_cross_include_file var name)
     PARENT_SCOPE)
 endfunction ()
 
-#=============================================================================
-# Configures the cmake files that describe how to cross compile paraview
-# From the ${cross_target} directory into the build tree.
-#
+# Configures the cmake files that describe how to crosscompile for a given
+# platform. From the ${SUPERBUILD_CROSS_TARGET} directory into the build tree.
 function (_superbuild_cross_platform_settings)
   set(site_toolchain
     "${CMAKE_SOURCE_DIR}/crosscompile/${SUPERBUILD_CROSS_TARGET}/toolchain.cmake.in")
