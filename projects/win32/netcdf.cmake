81480219e183ae8c5d46f75cd2461a6016113ff7
Ben Boeckel <ben.boeckel@kitware.com>
2019-03-15 13:36:51 -0400

    netcdf: update patches

--- projects/win32/netcdf.cmake
+++ projects/win32/netcdf.cmake
@@ -3,3 +3,7 @@ include("${CMAKE_CURRENT_LIST_DIR}/../netcdf.cmake")
 # Fix wrong name of win32 into _win32
 superbuild_apply_patch(netcdf fix-_win32-name
   "Replace win32 with _win32")
+
+# Fix installation rules on Windows
+superbuild_apply_patch(netcdf install-fixes
+  "Hide globbing install rules")
