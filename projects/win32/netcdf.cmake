659a93edc0b1c71c4ed78db44c7ce74f1d86aa90
Ben Boeckel <ben.boeckel@kitware.com>
2019-03-15 13:37:34 -0400

    netcdf: find shared libs the same way netcdf is built

--- projects/win32/netcdf.cmake
+++ projects/win32/netcdf.cmake
@@ -4,6 +4,10 @@ include("${CMAKE_CURRENT_LIST_DIR}/../netcdf.cmake")
 superbuild_apply_patch(netcdf fix-_win32-name
   "Replace win32 with _win32")
 
+# Fix linking to a shared hdf5 on Windows
+superbuild_apply_patch(netcdf find-shared-hdf5
+  "Link to HDF5 shared on Windows")
+
 # Fix installation rules on Windows
 superbuild_apply_patch(netcdf install-fixes
   "Hide globbing install rules")
