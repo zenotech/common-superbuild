b08dd4d8f9adbbd81f53230a8ffe148534894544
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2019-05-22 11:26:14 -0400

    zlib: update to zlib 1.2.11
    this version seems to include the `zlib-version-script.patch` and doesn't
    seem to need `zlib-remove-zconf.h.patch`.

--- projects/zlib.cmake
+++ projects/zlib.cmake
@@ -2,12 +2,3 @@ superbuild_add_project(zlib
   CAN_USE_SYSTEM
   CMAKE_ARGS
     -DBUILD_SHARED_LIBS:BOOL=${BUILD_SHARED_LIBS})
-
-# Remove the zconf.h file since the CMake build doesn't work with it.
-superbuild_apply_patch(zlib remove-zconf.h
-  "Remove zconf.h for the CMake build")
-
-# zlib needs a fix for '--version-script' linker option. The option needs to be
-# removed on Macs.
-superbuild_apply_patch(zlib version-script
-  "Remove --version-script linker argument on OS X")
