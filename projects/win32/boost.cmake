397452d5739d2a9e263b1ede3d90040cbb93cca2
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2019-05-22 11:48:11 -0400

    boost: update to 1.70.0

--- projects/win32/boost.cmake
+++ projects/win32/boost.cmake
@@ -34,12 +34,6 @@ endif ()
 
 include(boost.common)
 
-# This patch is applied so that a rogue junction is not left inside of the
-# build tree for dashboard machines. CMake cannot delete such files, so we just
-# remove the code which creates such things here.
-superbuild_apply_patch(boost no-junction-test
-  "Assume junctions work on Windows")
-
 superbuild_project_add_step(boost-copylibs
   COMMAND   "${CMAKE_COMMAND}"
             -Dinstall_location:PATH=<INSTALL_DIR>
