a9d9dd1ddf3f1b4dc421bcda0f98d54b6dc61f30
Ben Boeckel <ben.boeckel@kitware.com>
2019-11-07 11:01:31 -0500

    boost: support ignoring the junction patch if not needed

--- projects/win32/boost.cmake
+++ projects/win32/boost.cmake
@@ -37,8 +37,10 @@ include(boost.common)
 # This patch is applied so that a rogue junction is not left inside of the
 # build tree for dashboard machines. CMake cannot delete such files, so we just
 # remove the code which creates such things here.
-superbuild_apply_patch(boost no-junction-test
-  "Assume junctions work on Windows")
+if (NOT boost_no_junction_patch_necessary)
+  superbuild_apply_patch(boost no-junction-test
+    "Assume junctions work on Windows")
+endif ()
 
 superbuild_project_add_step(boost-copylibs
   COMMAND   "${CMAKE_COMMAND}"
