0890ad417b8e52be00c661b6b672528dad436d67
Ben Boeckel <ben.boeckel@kitware.com>
2020-01-08 10:27:00 -0500

    python2, python3: use the new find modules

--- projects/python3.system.cmake
+++ projects/python3.system.cmake
@@ -1,45 +1,42 @@
 option(python3_FIND_LIBRARIES "Require system Python development files" ON)
 mark_as_advanced(python3_FIND_LIBRARIES)
 
-find_package(PythonInterp 3.3 REQUIRED)
-
+set(python3_components
+  Interpreter)
 if (python3_FIND_LIBRARIES)
-  find_package(PythonLibs 3.3 REQUIRED)
-  if (NOT PYTHON_VERSION_STRING VERSION_EQUAL PYTHONLIBS_VERSION_STRING AND
-      NOT PYTHON_I_KNOW_WHAT_IM_DOING)
-    message(FATAL_ERROR
-      "Python interpreter version (${PYTHON_VERSION_STRING}) does "
-      "not match the library version (${PYTHONLIBS_VERSION_STRING})")
-  endif ()
+  list(APPEND python3_components
+    Development)
 endif()
 
-set(superbuild_python_version "${PYTHON_VERSION_MAJOR}.${PYTHON_VERSION_MINOR}"
+find_package(Python3 3.3 REQUIRED COMPONENTS ${python3_components})
+
+set(superbuild_python_version "${Python3_VERSION_MAJOR}.${Python3_VERSION_MINOR}"
   CACHE INTERNAL "")
 
 # This will add PYTHON_LIBRARY, PYTHON_EXECUTABLE, PYTHON_INCLUDE_DIR
 # variables. User can set/override these to change the Python being used.
 superbuild_add_extra_cmake_args(
-  -DPYTHON_EXECUTABLE:FILEPATH=${PYTHON_EXECUTABLE}
-  -DPython3_EXECUTABLE:FILEPATH=${PYTHON_EXECUTABLE})
+  -DPYTHON_EXECUTABLE:FILEPATH=${Python3_EXECUTABLE}
+  -DPython3_EXECUTABLE:FILEPATH=${Python3_EXECUTABLE})
 
 if (python3_FIND_LIBRARIES)
   superbuild_add_extra_cmake_args(
-    -DPYTHON_INCLUDE_DIR:PATH=${PYTHON_INCLUDE_DIR}
-    -DPYTHON_LIBRARY:FILEPATH=${PYTHON_LIBRARY}
-    -DPython3_INCLUDE_DIR:PATH=${PYTHON_INCLUDE_DIR}
-    -DPython3_LIBRARY:FILEPATH=${PYTHON_LIBRARY})
+    -DPYTHON_INCLUDE_DIR:PATH=${Python3_INCLUDE_DIR}
+    -DPYTHON_LIBRARY:FILEPATH=${Python3_LIBRARY}
+    -DPython3_INCLUDE_DIR:PATH=${Python3_INCLUDE_DIR}
+    -DPython3_LIBRARY:FILEPATH=${Python3_LIBRARY})
 endif()
 
-set(superbuild_python_executable "${PYTHON_EXECUTABLE}"
+set(superbuild_python_executable "${Python3_EXECUTABLE}"
   CACHE INTERNAL "")
 if (WIN32)
-  set(superbuild_python_pip "${PYTHON_EXECUTABLE};-m;pip"
+  set(superbuild_python_pip "${Python3_EXECUTABLE};-m;pip"
     CACHE INTERNAL "")
 else ()
   if (NOT EXISTS "${superbuild_python_pip}")
     unset(superbuild_python_pip CACHE)
   endif ()
-  get_filename_component(python3_executable_dir "${PYTHON_EXECUTABLE}" DIRECTORY)
+  get_filename_component(python3_executable_dir "${Python3_EXECUTABLE}" DIRECTORY)
   find_program(superbuild_python_pip
     NAMES pip
     HINTS "${python3_executable_dir}")
