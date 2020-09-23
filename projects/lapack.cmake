3f53a266a22c23d58630ad0c7ede8b267c2ffc0d
Ben Boeckel <ben.boeckel@kitware.com>
2020-09-23 12:55:25 -0400

    lapack: pass -fallow-argument-mismatch for gfortran 10+

--- projects/lapack.cmake
+++ projects/lapack.cmake
@@ -1,7 +1,18 @@
+set(lapack_environment)
+
+# Set `-fallow-argument-mismatch` for gfortran 10+.
+if (CMAKE_Fortran_COMPILER_ID STREQUAL "GNU" AND
+    NOT CMAKE_Fortran_COMPILER_VERSION VERSION_LESS "10")
+  list(APPEND lapack_environment
+    FFLAGS -fallow-argument-mismatch)
+endif ()
+
 superbuild_add_project(lapack
   CAN_USE_SYSTEM
   DEPENDS fortran
   CMAKE_ARGS
     -DBUILD_SHARED_LIBS:BOOL=${BUILD_SHARED_LIBS}
     -DBUILD_TESTING:BOOL=OFF
-    -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>)
+    -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
+  PROCESS_ENVIRONMENT
+    ${lapack_environment})
