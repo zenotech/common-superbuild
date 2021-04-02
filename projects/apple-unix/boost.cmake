366d6ce0f06ce8e8c73a43807d0fc4237a54b800
Ben Boeckel <ben.boeckel@kitware.com>
2021-04-02 10:39:23 -0400

    boost: move apple-specific patch to the right place

--- projects/apple-unix/boost.cmake
+++ projects/apple-unix/boost.cmake
@@ -37,4 +37,12 @@ if (APPLE)
     DEPENDEES install
     COMMENT   ""
     WORKING_DIRECTORY <INSTALL_DIR>/lib)
+
+  # Patch reqiured to build boost 1.71 on macOS 10.13
+  cmake_host_system_information(RESULT os_release QUERY OS_RELEASE)
+  if (os_release VERSION_GREATER "10.13" AND os_release VERSION_LESS "10.14")
+    superbuild_apply_patch(boost-iostreams pull103
+      "Patch boost for macOS 10.13"
+    )
+  endif()
 endif ()
