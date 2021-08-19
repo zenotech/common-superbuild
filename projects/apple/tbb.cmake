0a8bf67b46e5ba894d2c78304eef280b991723b5
Ben Boeckel <ben.boeckel@kitware.com>
2021-08-19 09:07:02 -0400

    tbb: patch the installed TBBConfig locations
    The superbuild moves the install artifacts around as far as `TBBConfig`
    is concerned, so the subdirectories that are expected to exist need to
    be masked out.

--- projects/apple/tbb.cmake
+++ projects/apple/tbb.cmake
@@ -1,2 +1,5 @@
 set(tbb_libdir "lib")
 include(tbb.common)
+
+superbuild_apply_patch(tbb macos-cmake-relocate
+  "Fix CMake configuration files for the install tree relocations")
--- projects/unix/tbb.cmake
+++ projects/unix/tbb.cmake
@@ -2,3 +2,6 @@ set(tbb_archdir intel64)
 set(tbb_libdir "lib/${tbb_archdir}/gcc4.8")
 set(tbb_libsuffix "${CMAKE_SHARED_LIBRARY_SUFFIX}*")
 include(tbb.common)
+
+superbuild_apply_patch(tbb linux-cmake-relocate
+  "Fix CMake configuration files for the install tree relocations")
