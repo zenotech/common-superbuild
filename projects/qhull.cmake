44978819cf1479ff17794e46469d4a1085919498
Ben Boeckel <ben.boeckel@kitware.com>
2021-08-25 20:59:54 -0400

    qhull: remove support for a system copy
    It doesn't actually have one here.

--- projects/qhull.cmake
+++ projects/qhull.cmake
@@ -4,7 +4,6 @@ if (BUILD_SHARED_LIBS)
 endif ()
 
 superbuild_add_project(qhull
-  CAN_USE_SYSTEM
   CMAKE_ARGS
     -DBUILD_SHARED_LIBS:BOOL=${BUILD_SHARED_LIBS}
     -DBUILD_STATIC_LIBS:BOOL=${qhull_build_static_libs}
