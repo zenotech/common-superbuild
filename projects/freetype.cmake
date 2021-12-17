87f63f3391cc1191ccf4df676ba492a5b2a74977
Ben Boeckel <ben.boeckel@kitware.com>
2021-12-16 21:36:10 -0500

    freetype: set the install rpath

--- projects/freetype.cmake
+++ projects/freetype.cmake
@@ -1,3 +1,9 @@
+set(freetype_install_rpath)
+if (UNIX AND NOT APPLE)
+  list(APPEND freetype_install_rpath
+    -DCMAKE_INSTALL_RPATH:STRING=<INSTALL_DIR>/lib)
+endif ()
+
 superbuild_add_project(freetype
   CAN_USE_SYSTEM
   DEPENDS zlib png
@@ -5,6 +11,7 @@ superbuild_add_project(freetype
     -DBUILD_SHARED_LIBS:BOOL=${BUILD_SHARED_LIBS}
     -DCMAKE_INSTALL_LIBDIR:STRING=lib
     -DCMAKE_INSTALL_NAME_DIR:PATH=<INSTALL_DIR>/lib
+    ${freetype_install_rpath}
     -DFT_WITH_ZLIB:BOOL=${zlib_enabled}
     -DFT_WITH_BZIP2:BOOL=OFF
     -DFT_WITH_PNG:BOOL=${png_enabled}
