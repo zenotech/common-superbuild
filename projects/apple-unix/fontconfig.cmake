edd5b30458e7cc0be789c99c9daf0da8cc066d05
Dan Lipsa <dan.lipsa@kitware.com>
2022-02-25 16:50:59 -0500

    Add PKG_CONFIG to fontconfig path.

--- projects/apple-unix/fontconfig.cmake
+++ projects/apple-unix/fontconfig.cmake
@@ -1,17 +1,29 @@
+set(fontconfig_process_environment)
 if (BUILD_SHARED_LIBS)
   set(fontconfig_shared_args --enable-shared --disable-static)
 else ()
   set(fontconfig_shared_args --disable-shared --enable-static)
 endif ()
 
-set(fontconfig_platform_deps)
+set(fontconfig_depends)
 if (NOT APPLE)
-  list(APPEND fontconfig_platform_deps
+  list(APPEND fontconfig_depends
     utillinux)
 endif ()
+if (APPLE OR UNIX)
+  list(APPEND fontconfig_depends
+    pkgconf)
+
+  if (pkgconf_enabled)
+    list(APPEND fontconfig_process_environment
+      PKG_CONFIG "${superbuild_pkgconf}"
+      PKG_CONFIG_PATH <INSTALL_DIR>/lib/pkgconfig)
+  endif ()
+endif ()
+
 
 superbuild_add_project(fontconfig
-  DEPENDS freetype libxml2 png gperf ${fontconfig_platform_deps}
+  DEPENDS freetype libxml2 png gperf ${fontconfig_depends}
   BUILD_IN_SOURCE 1
   CONFIGURE_COMMAND
     <SOURCE_DIR>/configure
@@ -28,7 +40,7 @@ superbuild_add_project(fontconfig
   INSTALL_COMMAND
     $(MAKE) install-exec install-pkgconfigDATA installdirs
   PROCESS_ENVIRONMENT
-    PKG_CONFIG_PATH <INSTALL_DIR>/lib/pkgconfig)
+    ${fontconfig_process_environment})
 
 superbuild_project_add_step(install-headers
   COMMAND   make install-fontconfigincludeHEADERS
