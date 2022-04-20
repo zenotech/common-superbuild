fdaa1b84bd7c2930c61043fdd73b48da95a25b3f
Mathieu Westphal <mathieu.westphal@kitware.com>
2022-04-20 14:25:24 +0200

    Adding utillinux license

--- projects/unix/utillinux.cmake
+++ projects/unix/utillinux.cmake
@@ -1,6 +1,9 @@
 # This project is a grabbag of random things. We only need libuuid for
 # fontconfig (as of now), so disable everything else.
 superbuild_add_project(utillinux
+  LICENSE_FILES
+    libuuid/COPYING # This is the license of the code that is installed
+    Documentation/licenses/COPYING.BSD-3-Clause
   CONFIGURE_COMMAND
     <SOURCE_DIR>/configure
       --prefix=<INSTALL_DIR>
@@ -32,3 +35,6 @@ superbuild_apply_patch(utillinux uuid-incdir
 
 superbuild_apply_patch(utillinux fix-libuuid-restrict-keyword
   "Fix restrict keyword compile error on older versions of gcc. Remove after 2.37.3+ when this patch makes it into the release. See https://github.com/util-linux/util-linux/issues/1405 for additional information")
+
+superbuild_apply_patch(utillinux remove-terminal-colors
+  "Remove terminal colors from installation as it is a GPL licensed file which we do net need at all")
