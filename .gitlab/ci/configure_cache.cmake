e8851946fd6a758e5d227f5c604deea23753023b
Ben Boeckel <ben.boeckel@kitware.com>
2022-07-03 19:26:03 -0400

    ci: use buildcache instead of sccache on Windows

--- .gitlab/ci/configure_cache.cmake
+++ .gitlab/ci/configure_cache.cmake
@@ -1,11 +1,11 @@
 set(qt5_SKIP_PCH "ON" CACHE BOOL "")
 
 if ("$ENV{CI_JOB_NAME}" MATCHES "windows")
+  set(CMAKE_C_COMPILER_LAUNCHER "buildcache" CACHE STRING "")
+  set(CMAKE_CXX_COMPILER_LAUNCHER "buildcache" CACHE STRING "")
   set(superbuild_replace_uncacheable_flags ON CACHE BOOL "")
-endif ()
-
 # Linux replaces `CC` and `CXX`
-if (NOT "$ENV{CI_JOB_NAME}" MATCHES "linux")
+elseif (NOT "$ENV{CI_JOB_NAME}" MATCHES "linux")
   set(CMAKE_C_COMPILER_LAUNCHER "sccache" CACHE STRING "")
   set(CMAKE_CXX_COMPILER_LAUNCHER "sccache" CACHE STRING "")
 endif ()
