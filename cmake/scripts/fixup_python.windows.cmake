d0d7dbab16a1b3bb00666fd4da98f20e4c470bfc
Ben Boeckel <ben.boeckel@kitware.com>
2022-08-31 11:08:54 -0400

    fixup_python: avoid directory shadowing problems
    Packages can ship with the following layout:
    
      package/
        module.py
        module/resources
    
    The old search pattern would be told "look for `package.module` and try
    the extensions to install what is needed. However, this would end up
    installing `module.py` twice: once when `package/module/` is found and
    again when `package/module.py` is found.
    
    This is normally fine as a double install would just replace the same
    content. However, if the directory is named `MODULE` and the filesystem
    is case-insensitive (such as macOS and Windows), `module.py` gets
    installed as `MODULE.py`. When the second install comes around, the
    case-preserving nature of these filesystems will keep the uppercase
    variant. This ends up causing all kinds of fun later.
    
    To fix this:
    
      - check for directory-ness first
      - if a file was found, skip the directory check
    
    See: paraview/paraview-superbuild#223

--- cmake/scripts/fixup_python.windows.cmake
+++ cmake/scripts/fixup_python.windows.cmake
@@ -7,30 +7,6 @@ function (superbuild_windows_install_python_module destination module search_pat
     ${local_extra_search_arguments})
 
   foreach (search_path IN LISTS search_paths)
-    if (EXISTS "${search_path}/${module}.py")
-      file(INSTALL
-        FILES       "${search_path}/${module}.py"
-        DESTINATION "${destination}/${location}")
-    endif ()
-
-    if (EXISTS "${search_path}/${module}.zip")
-      file(INSTALL
-        FILES       "${search_path}/${module}.zip"
-        DESTINATION "${destination}/${location}")
-    endif()
-
-    if (EXISTS "${search_path}/${module}.pyd")
-      set(module_pyd "${search_path}/${module}.pyd")
-    else ()
-      # some modules have names that are prefixed with build specific extension
-      # e.g. `kiwisolver.cpython-37m-x86_64.pyd`, so we do a glob if
-      # direct lookup fails.
-      file(GLOB module_pyd LIST_DIRECTORIES false "${search_path}/${module}.*.pyd")
-      if (module_pyd)
-        list(GET module_pyd 0 module_pyd)
-      endif()
-    endif ()
-
     # Python wheels ship their dependencies in `.libs` directories.
     if (EXISTS "${search_path}/.libs")
       # Add it to the search path if it exists.
@@ -42,51 +18,77 @@ function (superbuild_windows_install_python_module destination module search_pat
       set(local_extra_search_arguments "")
     endif ()
 
-    if (EXISTS "${module_pyd}")
-      execute_process(
-        COMMAND "${superbuild_python_executable}"
-                "${_superbuild_install_cmake_scripts_dir}/fixup_bundle.windows.py"
-                --destination "${bundle_destination}"
-                ${fixup_bundle_arguments}
-                ${local_extra_search_arguments}
-                ${parent_extra_search_arguments}
-                --location    "${location}"
-                --manifest    "${bundle_manifest}"
-                --type        module
-                --libdir      "${location}"
-                "${module_pyd}"
-        RESULT_VARIABLE res
-        ERROR_VARIABLE  err)
-
-      if (res)
-        message(FATAL_ERROR "Failed to install Python module ${module}:\n${err}")
-      endif ()
-    endif ()
-    if (IS_DIRECTORY "${search_path}/${module}")
+    if (IS_DIRECTORY "${search_path}/${module}" AND NOT _superbuild_python_module_is_not_dir)
       file(GLOB contents RELATIVE "${search_path}/${module}" "${search_path}/${module}/*")
       foreach (item_name IN LISTS contents)
         set(item_path "${search_path}/${module}/${item_name}")
         if (IS_DIRECTORY "${item_path}")
           if (NOT item_name STREQUAL "__pycache__")
+            set(_superbuild_python_module_is_not_dir FALSE)
             superbuild_windows_install_python_module("${destination}"
               "${item_name}" "${search_path}/${module}" "${location}/${module}")
-          endif()
-        else()
+          endif ()
+        else ()
           # not a directory, check if it's a Python module or arbitrary artifact
           # file.
           if (item_name MATCHES "\\.(py|pyd)$")
             # it's a Python module; install it by a recursive call.
             string(REGEX REPLACE "\\.(py|pyd)$" "" item_name "${item_name}")
+            set(_superbuild_python_module_is_not_dir TRUE)
             superbuild_windows_install_python_module("${destination}"
               "${item_name}" "${search_path}/${module}" "${location}/${module}")
-          else()
+          else ()
             # an artifact file; install it.
             file(INSTALL
               FILES       "${search_path}/${module}/${item_name}"
               DESTINATION "${destination}/${location}/${module}")
-          endif()
+          endif ()
         endif ()
-      endforeach()
+      endforeach ()
+
+    elseif (EXISTS "${search_path}/${module}.py")
+      file(INSTALL
+        FILES       "${search_path}/${module}.py"
+        DESTINATION "${destination}/${location}")
+
+    elseif (EXISTS "${search_path}/${module}.zip")
+      file(INSTALL
+        FILES       "${search_path}/${module}.zip"
+        DESTINATION "${destination}/${location}")
+
+    else ()
+      if (EXISTS "${search_path}/${module}.pyd")
+        set(module_pyd "${search_path}/${module}.pyd")
+      else ()
+        # some modules have names that are prefixed with build specific extension
+        # e.g. `kiwisolver.cpython-37m-x86_64.pyd`, so we do a glob if
+        # direct lookup fails.
+        file(GLOB module_pyd LIST_DIRECTORIES false "${search_path}/${module}.*.pyd")
+        if (module_pyd)
+          list(GET module_pyd 0 module_pyd)
+        endif()
+      endif ()
+
+      if (EXISTS "${module_pyd}")
+        execute_process(
+          COMMAND "${superbuild_python_executable}"
+                  "${_superbuild_install_cmake_scripts_dir}/fixup_bundle.windows.py"
+                  --destination "${bundle_destination}"
+                  ${fixup_bundle_arguments}
+                  ${local_extra_search_arguments}
+                  ${parent_extra_search_arguments}
+                  --location    "${location}"
+                  --manifest    "${bundle_manifest}"
+                  --type        module
+                  --libdir      "${location}"
+                  "${module_pyd}"
+          RESULT_VARIABLE res
+          ERROR_VARIABLE  err)
+
+        if (res)
+          message(FATAL_ERROR "Failed to install Python module ${module}:\n${err}")
+        endif ()
+      endif ()
     endif ()
   endforeach ()
 endfunction ()
