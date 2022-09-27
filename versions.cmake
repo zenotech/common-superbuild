18a259f3946c92cec9009ee24336c293a611b7f8
Mathieu Westphal <mathieu.westphal@kitware.com>
2022-09-27 12:17:44 +0800

    Adding OSPray stack to common-superbuild
    Copied from paraview-superbuild, adding OSPray and all related projects
    to the common-superbuild

--- versions.cmake
+++ versions.cmake
@@ -351,6 +351,48 @@ superbuild_set_revision(jsoncpp
   URL     "https://www.paraview.org/files/dependencies/jsoncpp-1.9.5.tar.gz"
   URL_MD5 d6c8c609f2162eff373db62b90a051c7)
 
+if (WIN32)
+  set(ispc_suffix "windows.zip")
+  set(ispc_md5 22d1e9fd03427b8e8a9d75ce56cfa495)
+elseif (APPLE)
+  set(ispc_suffix "macOS.tar.gz")
+  set(ispc_md5 0ae980be5d319b38592c6ef5596c305d)
+else()
+  set(ispc_suffix "linux.tar.gz")
+  set(ispc_md5 4665c577541003e31c8ce0afd64b6952)
+endif()
+superbuild_set_revision(ispc
+  URL     "https://www.paraview.org/files/dependencies/ispc-v1.16.1-${ispc_suffix}"
+  URL_MD5 "${ispc_md5}")
+
+superbuild_set_revision(embree
+  URL     "https://www.paraview.org/files/dependencies/embree-v3.13.1.tar.gz"
+  URL_MD5 71453f1e9af48a95090112e493982898)
+
+superbuild_set_revision(openvkl
+  URL     "https://www.paraview.org/files/dependencies/openvkl-v1.0.1.tar.gz"
+  URL_MD5 c6a9a222df0e7f21b49ea8081b509171)
+
+superbuild_set_revision(ospray
+  URL     "https://www.paraview.org/files/dependencies/ospray-v2.7.1.tar.gz"
+  URL_MD5 e027ca7a5119300a0c94c3f9be38b58d)
+
+superbuild_set_revision(ospraymaterials
+  URL     "https://www.paraview.org/files/data/OSPRayMaterials-0.3.tar.gz"
+  URL_MD5 d256c17f70890d3477e90d35bf814c25)
+
+superbuild_set_revision(openimagedenoise
+  URL     "https://www.paraview.org/files/dependencies/oidn-1.4.1.src.tar.gz"
+  URL_MD5 df4007b0ab93b1c41cdf223b075d01c0)
+
+superbuild_set_revision(rkcommon
+  URL     "https://www.paraview.org/files/dependencies/rkcommon-v1.7.0.tar.gz"
+  URL_MD5 1bd26e5aea9b1c4873fe8b8cec9a1d28)
+
+superbuild_set_revision(snappy
+  URL     "https://www.paraview.org/files/dependencies/snappy-1.1.9.tar.gz"
+  URL_MD5 213b6324b7790d25f5368629540a172c)
+
 set(pythoncryptography_version "3.4.7")
 if (CMAKE_SYSTEM_NAME STREQUAL "Windows")
   if (CMAKE_HOST_SYSTEM_PROCESSOR MATCHES "64")
