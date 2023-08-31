b828ca89b1451732e0a2ad0233af4a6d40d9f664
Ben Boeckel <ben.boeckel@kitware.com>
2023-08-31 09:14:22 -0400

    pythoncffi: depend on ffi

--- projects/pythoncffi.cmake
+++ projects/pythoncffi.cmake
@@ -1,4 +1,10 @@
+set(pythoncffi_depends)
+if (UNIX)
+  list(APPEND pythoncffi_depends
+    pkgconf ffi)
+endif ()
+
 superbuild_add_project_python(pythoncffi
   PACKAGE cffi
-  DEPENDS pythonsetuptools
+  DEPENDS pythonsetuptools ${pythoncffi_depends}
   LICENSE_FILES LICENSE)
