c9bff6f5253087a96cf4b24c59c7463565eb19cf
Ben Boeckel <ben.boeckel@kitware.com>
2023-09-07 10:49:47 -0400

    szip: avoid @rpath/ library ids

--- projects/szip.cmake
+++ projects/szip.cmake
@@ -2,6 +2,7 @@ superbuild_add_project(szip
   LICENSE_FILES
     COPYING
   CMAKE_ARGS
+    -DCMAKE_INSTALL_NAME_DIR:STRING=<INSTALL_DIR>/lib
     -DSZIP_ENABLE_ENCODING:BOOL=OFF # This option should not be changed as we do not distribute the encoding/compression license
     -DSZIP_INSTALL_DATA_DIR:STRING=share
     -DSZIP_INSTALL_CMAKE_DIR:STRING=lib/cmake/SZIP
