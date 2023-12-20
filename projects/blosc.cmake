c3a0f70b5e69586f79767b38c15152c643f1d1ea
Mathieu Westphal <mathieu.westphal@kitware.com>
2023-12-20 08:19:02 +0100

    blosc: making lz4, zstd, zlib optional

--- projects/blosc.cmake
+++ projects/blosc.cmake
@@ -3,10 +3,25 @@ if (BUILD_SHARED_LIBS)
   set(blosc_static_libs OFF)
 endif ()
 
+set(blosc_without_lz4 ON)
+if (lz4_enabled)
+  set(blosc_without_lz4 OFF)
+endif ()
+
+set(blosc_without_zlib ON)
+if (zlib_enabled)
+  set(blosc_without_zlib OFF)
+endif ()
+
+set(blosc_without_zstd ON)
+if (zstd_enabled)
+  set(blosc_without_zstd OFF)
+endif ()
+
 superbuild_add_project(blosc
   CAN_USE_SYSTEM
-  DEPENDS
-    zlib zstd
+  DEPENDS_OPTIONAL
+    zlib zstd lz4
   LICENSE_FILES
     LICENSE.txt
     LICENSES/LZ4.txt
@@ -15,9 +30,7 @@ superbuild_add_project(blosc
   SPDX_COPYRIGHT_TEXT
     "Copyright (c) 2009-2018 Francesc Alted"
     "Copyright (c) 2019-present Blosc Development Team"
-    "Copyright (c) 2011-2020, Yann Collet" # LZ4
   CMAKE_ARGS
-    -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
     -DCMAKE_INSTALL_LIBDIR:STRING=lib
     -DCMAKE_INSTALL_NAME_DIR:PATH=<INSTALL_DIR>/lib
     -DBUILD_SHARED:BOOL=${BUILD_SHARED_LIBS}
@@ -25,11 +38,10 @@ superbuild_add_project(blosc
     -DBUILD_TESTS:BOOL=OFF
     -DBUILD_FUZZERS:BOOL=OFF
     -DBUILD_BENCHMARKS:BOOL=OFF
-    # good addition, but uses Makefiles: http://lz4.github.io/lz4/
-    -DDEACTIVATE_LZ4:BOOL=OFF
+    -DDEACTIVATE_LZ4:BOOL=${blosc_without_lz4}
     -DDEACTIVATE_SNAPPY:BOOL=ON
-    -DDEACTIVATE_ZLIB:BOOL=ON
-    -DDEACTIVATE_ZSTD:BOOL=ON
-    -DPREFER_EXTERNAL_LZ4:BOOL=ON
+    -DDEACTIVATE_ZLIB:BOOL=${blosc_without_zlib}
+    -DDEACTIVATE_ZSTD:BOOL=${blosc_without_zstd}
+    -DPREFER_EXTERNAL_LZ4:BOOL=${lz4_enabled}
     -DPREFER_EXTERNAL_ZLIB:BOOL=${zlib_enabled}
     -DPREFER_EXTERNAL_ZSTD:BOOL=${zstd_enabled})
