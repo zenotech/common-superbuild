ac99c5f1bf6389a2e794eedd9593f6e1fcc59a40
Ben Boeckel <ben.boeckel@kitware.com>
2024-01-14 16:43:30 -0500

    python3: support finding `pip` from a virtualenv

--- projects/python3.system.cmake
+++ projects/python3.system.cmake
@@ -50,5 +50,6 @@ else ()
     NAMES "pip-${superbuild_python_version}" "pip${superbuild_python_version}"
           pip-3 pip3
           pip
-    HINTS "${python3_executable_dir}")
+    HINTS "$ENV{VIRTUAL_ENV}/bin"
+          "${python3_executable_dir}")
 endif ()
