ac2f36b301a9f61e90c620efcba5ab1f4dd3df55
Mathieu Westphal <mathieu.westphal@kitware.com>
2024-03-04 09:41:20 +0100

    CI: Explicitely disable qt5 Multimedia on Windows and macOS

--- .gitlab/ci/configure_windows.cmake
+++ .gitlab/ci/configure_windows.cmake
@@ -1,4 +1,7 @@
 file(TO_CMAKE_PATH "$ENV{CI_PROJECT_DIR}/.gitlab/qt" cmake_qt_prefix)
 set(CMAKE_PREFIX_PATH "${cmake_qt_prefix}" CACHE STRING "")
 
+set(qt5_ENABLE_MULTIMEDIA "OFF" CACHE STRING "") # Not installed on Windows CI
+set(qt5_ENABLE_WEBENGINE "OFF" CACHE STRING "") # Not installed on Windows CI
+
 include("${CMAKE_CURRENT_LIST_DIR}/configure_common.cmake")
