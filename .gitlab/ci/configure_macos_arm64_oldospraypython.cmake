df2466e39abaeda313d2504f2f600ac7d8da7148
Ben Boeckel <ben.boeckel@kitware.com>
2024-11-21 14:22:26 +0100

    ci: disable projects which need updated to support newer toolchains

--- .gitlab/ci/configure_macos_arm64_oldospraypython.cmake
+++ .gitlab/ci/configure_macos_arm64_oldospraypython.cmake
@@ -1,5 +1,9 @@
 set(CMAKE_OSX_DEPLOYMENT_TARGET "11.0" CACHE STRING "")
 
+# Turn off projects which need patches to work with newer Xcode versions.
+set(ENABLE_numpy OFF CACHE BOOL "")
+set(ENABLE_scipy OFF CACHE BOOL "")
+
 # Add the rpath for fortran libraries.
 set(_superbuild_fortran_ld_flags "-Wl,-rpath,$ENV{CI_PROJECT_DIR}/.gitlab/gfortran/lib" CACHE STRING "")
 
